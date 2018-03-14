35d1bd7760037ae134880008dcd16a4384088a7b
Maros Tkacik <maros.tkacik9@gmail.com>
2018-03-14 18:34:11 +0100

    rename lock-free node to simple_node

---
+++ node/lockfree/simple_node.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <bits/stdc++.h>
+
+namespace parallel_union_find::lockfree
+{
+    class simple_node
+    {
+        public:
+            simple_node();
+
+            simple_node* find_set();
+            bool same_set(simple_node*);
+            bool union_set(simple_node*);
+        private:
+            std::atomic<bool> _lock;
+            std::atomic<uint64_t> _size;
+            std::atomic<simple_node*> _parent;
+    };
+
+    #include "simple_node.tpp"
+}
