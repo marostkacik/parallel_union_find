3962b0e0a898737dd0b7993b60bb1473f05d3bf5
Maros Tkacik <maros.tkacik9@gmail.com>
2018-03-14 18:13:17 +0100

    remove usings, type of node to union find is decided by user

--- lockfree/union_find/union_find.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "union_find/union_find.hpp"
-#include "lockfree/node/node.hpp"
-
-namespace parallel_union_find::lockfree
-{
-    using union_find = parallel_union_find::union_find<node>;
-}
--- waitfree/union_find/union_find.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "union_find/union_find.hpp"
-#include "waitfree/node/node.hpp"
-
-namespace parallel_union_find::waitfree
-{
-    using union_find = parallel_union_find::union_find<node>;
-}
