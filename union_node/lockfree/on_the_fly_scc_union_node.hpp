84d5a574d48b21fffe160477130f3b731949cdf2
Maros Tkacik <maros.tkacik9@gmail.com>
2018-04-12 21:42:34 +0200

    don't skip lines between functions

--- union_node/lockfree/on_the_fly_scc_union_node.hpp
+++ union_node/lockfree/on_the_fly_scc_union_node.hpp
@@ -12,32 +12,24 @@ public:
     // observers
     template<typename Node>
     static Node* find_set(Node const *);
-
     template<typename Node>
     static bool same_set(Node const *, Node const *);
-
     template<typename Node>
     static bool has_mask(Node const *, uint64_t);
-
     template<typename Node>
     static bool is_dead(Node const *);
-
     template<typename Node>
     static bool is_done(Node const *);
-
     template<typename Node>
     static Node* get_node_from_set(Node const *);
 
     // mutators
     template<typename Node>
     static bool union_set(Node*, Node*);
-
     template<typename Node>
     static void add_mask(Node*, uint64_t);
-
     template<typename Node>
     static bool mark_as_dead(Node*);
-
     template<typename Node>
     static bool mark_as_done(Node*);
 
@@ -45,13 +37,10 @@ private:
     // helper functions
     template<typename Node>
     static bool is_top(Node const *);
-
     template<typename Node>
     static bool lock(Node const *);
-
     template<typename Node>
     static void unlock(Node const *);
-
     template<typename Node>
     static void hook_under_me(Node*, Node*);
 
