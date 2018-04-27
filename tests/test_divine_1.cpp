d071387019ef97eda307b6353d68f89f2337a019
Maros Tkacik <maros.tkacik9@gmail.com>
2018-04-27 14:00:14 +0200

    add divine tests

---
+++ tests/test_divine_1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include "union_node/lockfree/on_the_fly_scc_union_node.hpp"
+#include "graph_node/concurrent_graph_node.hpp"
+#include "algorithm/concurrent_algorithm.hpp"
+
+using namespace std;
+using namespace parallel_union_find::union_node::lockfree;
+using namespace parallel_union_find::graph_node;
+using namespace parallel_union_find::algorithm;
+
+using node = concurrent_graph_node<on_the_fly_scc_union_node>;
+
+vector<node> nodes(4);
+
+int main()
+{
+    // set labels
+    for (int i = 0; i < 4; ++i)
+        nodes.at(i).set_label(i);
+
+    // set edges 0 -> 1, 1 -> 2, 2 -> 3, 3 -> 3, 3 -> 0
+    nodes.at(0).add_son(&nodes.at(1));
+    nodes.at(1).add_son(&nodes.at(2));
+    nodes.at(2).add_son(&nodes.at(3));
+    nodes.at(3).add_son(&nodes.at(3));
+    nodes.at(3).add_son(&nodes.at(0));
+
+    // run
+    const int num_threads = 2;
+    vector<thread> threads;
+
+    for (int i = 0; i < num_threads; ++i)
+        threads.emplace_back(concurrent_algorithm<node>, &nodes.at(0), (1 << i));
+    for (thread& t : threads)
+        t.join();
+
+    // print answer
+    for (int i = 0; i < 4; ++i)
+        cout << "dad of " << i << " is " << nodes.at(i).find_set()->get_label() << endl;
+}
+
