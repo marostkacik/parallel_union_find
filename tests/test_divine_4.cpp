4777cbbfdb401cba275d518d1be6ec1a4fc51a7e
Maros Tkacik <maros.tkacik9@gmail.com>
2018-05-14 22:19:06 +0200

    rework test_divine_3 to new union node API

--- tests/test_divine_4.cpp
+++ tests/test_divine_4.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <iostream>
 #include <cassert>
+#include <cstdint>
 #include "union_node/lockfree/on_the_fly_scc_union_node.hpp"
 #include "graph_node/concurrent_graph_node.hpp"
 #include "algorithm/concurrent_algorithm.hpp"
@@ -15,6 +16,21 @@ using node = concurrent_graph_node<on_the_fly_scc_union_node>;
 
 vector<node> nodes(2);
 
+bool same_set(node* n1, node* n2)
+{
+    std::pair<bool, bool> ans = n1->same_set(n2);
+
+    while (!ans.second)
+        ans = n1->same_set(n2);
+
+    return ans.first;
+}
+
+void add_mask(node* n, uint64_t mask)
+{
+    while (!n->add_mask(mask));
+}
+
 int main()
 {
     // set labels
@@ -27,22 +43,22 @@ int main()
 
 
     std::thread t1([](){
-        nodes.at(0).add_mask(1);
+        add_mask(&nodes.at(0), 1);
         assert(nodes.at(0).has_mask(1));
 
         while (!nodes.at(0).union_set(&nodes.at(1)));
 
-        assert(nodes.at(1).same_set(&nodes.at(0)));
+        assert(same_set(&nodes.at(1), &nodes.at(0)));
         assert(nodes.at(0).has_mask(1));
         assert(nodes.at(1).has_mask(1));
     });
     std::thread t2([](){
-        nodes.at(0).add_mask(2);
+        add_mask(&nodes.at(0), 2);
         assert(nodes.at(0).has_mask(2));
 
         while (!nodes.at(1).union_set(&nodes.at(0)));
 
-        assert(nodes.at(0).same_set(&nodes.at(1)));
+        assert(same_set(&nodes.at(0), &nodes.at(1)));
         assert(nodes.at(0).has_mask(2));
         assert(nodes.at(1).has_mask(2));
     });
@@ -50,7 +66,7 @@ int main()
     t1.join();
     t2.join();
 
-    assert(nodes.at(1).same_set(&nodes.at(0)));
-    assert(nodes.at(0).same_set(&nodes.at(1)));
+    assert(same_set(&nodes.at(1), &nodes.at(0)));
+    assert(same_set(&nodes.at(0), &nodes.at(1)));
 }
 
