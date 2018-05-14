8661390eb7526154a45075b6455881ac3e7f4824
Maros Tkacik <maros.tkacik9@gmail.com>
2018-05-14 22:01:33 +0200

    rework test_divine_2 to new union node API

--- tests/test_divine_2.cpp
+++ tests/test_divine_2.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <iostream>
 #include <cassert>
+#include <cstdint>
 #include "union_node/lockfree/on_the_fly_scc_union_node.hpp"
 #include "graph_node/concurrent_graph_node.hpp"
 #include "algorithm/concurrent_algorithm.hpp"
@@ -14,6 +15,21 @@ using namespace parallel_union_find::algorithm;
 using node = concurrent_graph_node<on_the_fly_scc_union_node>;
 
 vector<node> nodes(4);
+
+bool same_set(node* n1, node* n2)
+{
+    std::pair<bool, bool> ans = n1->same_set(n2);
+
+    while (!ans.second)
+        ans = n1->same_set(n2);
+
+    return ans.first;
+}
+
+void add_mask(node* n, uint64_t mask)
+{
+    while (!n->add_mask(mask));
+}
 
 int main()
 {
@@ -32,8 +48,8 @@ int main()
     std::thread t1([](){
         while (!nodes.at(0).union_set(&nodes.at(1)));
 
-        assert(nodes.at(0).same_set(&nodes.at(1)));
-        nodes.at(1).add_mask(1);
+        assert(same_set(&nodes.at(0), &nodes.at(1)));
+        add_mask(&nodes.at(1), 1);
 
         for (int i = 0; i < 5; ++i)
             if (i == 3)
@@ -46,8 +62,8 @@ int main()
     std::thread t2([](){
         while (!nodes.at(0).union_set(&nodes.at(2)));
 
-        assert(nodes.at(0).same_set(&nodes.at(2)));
-        nodes.at(2).add_mask(2);
+        assert(same_set(&nodes.at(0), &nodes.at(2)));
+        add_mask(&nodes.at(2), 2);
 
         for (int i = 0; i < 5; ++i)
             if (i == 3)
