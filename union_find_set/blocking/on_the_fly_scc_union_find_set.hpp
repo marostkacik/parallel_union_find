263658eaae5924dd67df948787055d3ea783d521
Maros Tkacik <maros.tkacik9@gmail.com>
2018-05-21 12:32:25 +0200

    update on_the_fly_scc_union_find_set

--- union_find_set/blocking/on_the_fly_scc_union_find_set.hpp
+++ union_find_set/blocking/on_the_fly_scc_union_find_set.hpp
@@ -3,51 +3,51 @@
 #include <atomic>
 #include <cassert>
 
-namespace parallel_union_find::union_set
+namespace parallel_union_find::union_find_set::blocking
 {
-    class on_the_fly_scc_union_node
+    class on_the_fly_scc_union_find_set
     {
     public:
-        on_the_fly_scc_union_node();
+        on_the_fly_scc_union_find_set();
 
         // observers
-        on_the_fly_scc_union_node* find_set() const;
-        bool                       same_set(on_the_fly_scc_union_node const *) const;
-        bool                       has_mask(uint64_t) const;
-        bool                       is_dead() const;
-        bool                       is_done() const;
-        on_the_fly_scc_union_node* get_node_from_set() const;
+        on_the_fly_scc_union_find_set* find_set() const;
+        bool                           same_set(on_the_fly_scc_union_find_set const *) const;
+        bool                           has_mask(uint64_t) const;
+        bool                           is_dead() const;
+        bool                           is_done() const;
+        on_the_fly_scc_union_find_set* get_node_from_set() const;
 
         // mutators
-        bool                       union_set(on_the_fly_scc_union_node*);
-        void                       add_mask(uint64_t);
-        bool                       mark_as_dead();
-        bool                       mark_as_done();
+        bool                           union_set(on_the_fly_scc_union_find_set*);
+        void                           add_mask(uint64_t);
+        bool                           mark_as_dead();
+        bool                           mark_as_done();
 
     private:
-        bool                       is_top() const;
+        bool                           is_top() const;
 
-        bool                       lock() const;
-        void                       unlock() const;
+        bool                           lock() const;
+        void                           unlock() const;
 
-        on_the_fly_scc_union_node* get_node_from_set_not_locking();
-        void                       hook_under_me(on_the_fly_scc_union_node* other);
+        on_the_fly_scc_union_find_set* get_node_from_set_not_locking();
+        void                           hook_under_me(on_the_fly_scc_union_find_set* other);
 
     private:
-        mutable std::atomic<bool>                       _spin_lock;
-        std::atomic<bool>                               _dead;
-        std::atomic<bool>                               _done;
+        mutable std::atomic<bool>                           _spin_lock;
+        std::atomic<bool>                                   _dead;
+        std::atomic<bool>                                   _done;
 
         // union set data
-        mutable std::atomic<on_the_fly_scc_union_node*> _parent;
-        std::atomic<uint64_t>                           _mask;
-        std::atomic<uint64_t>                           _size;
-        std::atomic<bool>                               _blocked;
+        mutable std::atomic<on_the_fly_scc_union_find_set*> _parent;
+        std::atomic<uint64_t>                               _mask;
+        std::atomic<uint64_t>                               _size;
+        std::atomic<bool>                                   _blocked;
 
         // circular linked list data
-        mutable std::atomic<on_the_fly_scc_union_node*> _start_node;
-        mutable std::atomic<on_the_fly_scc_union_node*> _next_node;
+        mutable std::atomic<on_the_fly_scc_union_find_set*> _start_node;
+        mutable std::atomic<on_the_fly_scc_union_find_set*> _next_node;
     };
 
-#include "on_the_fly_scc_union_node.tpp"
+#include "on_the_fly_scc_union_find_set.tpp"
 }
